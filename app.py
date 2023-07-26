8f086737c5cf815d8f9738846509592d5cd3b456
Francisco José Ruiz Vera <francisco@regionalseguros.com.py>
2023-07-26 14:13:21 -0400

    Prompt adjustment

--- app.py
+++ app.py
@@ -14,10 +14,13 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 st.title("Comparador de Contratos")
 st.write('''A continuación introduzca el texto de los contratos que desee comparar,
           cada uno en su respectivo campo, luego presione el botón "Comparar" y 
-         aguarde por la respuesta de la IA. Este proceso podría demorar unos minutos''')
+         aguarde por la respuesta de la IA. Este proceso podría demorar unos minutos.''')
+st.caption('''Por el momento la aplicación se encuentra limitada a 1000 palabras en cada campo, 
+           por lo que podría utilizarse para comparar exclusivamente las secciones de exclusión 
+           o coberturas de los contratos.''')
 
-text1 = st.text_area("Exclusiones del 1er Contrato", height=200)
-text2 = st.text_area("Exclusiones del 2do Contrato", height=200)
+text1 = st.text_area("Texto del 1er Contrato", height=200)
+text2 = st.text_area("Texto del 2do Contrato", height=200)
 compare = st.button("Comparar")
 
 def comparar_contratos(text1, text2):
@@ -31,14 +34,15 @@ def comparar_contratos(text1, text2):
                 se escriban diferente pero expresen lo mismo. Señálame las diferencias
                 y similitudes entre las expresiones que se escriben diferente pero
                 significan lo mismo. Explica porque son diferentes o iguales en cada caso.
-                Indicame si en general los textos son diferentes o si expresan lo mismo
+                Si la diferencia en la redacción es mínima y no parece afectar significativamente 
+                el significado de la frase, responde "No hay diferencia significativa".
               """.format(text1=text1, text2=text2)}
           ]
       )
-    return response["choices"][0]["message"]["content"] + "Total de Tokens utilizados en esta consulta: " +  str(response["usage"]["total_tokens"])
+    return response["choices"][0]["message"]["content"] + "\n Total de Tokens utilizados en esta consulta: " +  str(response["usage"]["total_tokens"])
 
 
 if compare:
     st.write(comparar_contratos(text1, text2))
 else:
-    st.write('Aquí se mostrará la respuesta de la IA')
+    st.caption('Aquí se mostrará la respuesta de la IA')
